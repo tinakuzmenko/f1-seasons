1560ac85ce5f7389efbb83a9691203e3fe149287
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2021-12-02 23:29:50 +0100

    Refactoring

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,8 +1,9 @@
 import Title from '../UI/Title/Title';
 import {useEffect, useState} from 'react';
+import {CenteredContent} from '../UI/Grid/GridRow/styles';
 import GridLayout from '../UI/Grid/GridLayout/GridLayout';
 import FavoriteDriver from './FavoriteDriver/FavoriteDriver';
-import {CenteredContent} from '../UI/Grid/GridRow/styles';
+import FavoritesHeader from './FavoritesHeader/FavoritesHeader';
 
 const Favorites = () => {
   const [favorites, setFavorites] = useState([]);
@@ -27,6 +28,7 @@ const Favorites = () => {
       <Title title='Favorites'/>
       {!favorites.length && <CenteredContent>Your favorites list is empty.</CenteredContent>}
       <GridLayout>
+        <FavoritesHeader/>
         {favorites.map((favorite) => <FavoriteDriver key={favorite} driver={favorite} onRemove={removeClickHandler}/>)}
       </GridLayout>
     </>
---
+++ src/components/Favorites/FavoritesHeader/FavoritesHeader.jsx
@@ -0,0 +1,17 @@
+import {FavoritesHeaderWrapper} from './styles';
+
+const FavoritesHeader = () => {
+  return (
+    <FavoritesHeaderWrapper>
+      <div>Code</div>
+      <div>Driver</div>
+      <div>Date of birth</div>
+      <div>Nationality</div>
+      <div>Perm. #</div>
+      <div></div>
+      <div></div>
+    </FavoritesHeaderWrapper>
+  )
+}
+
+export default FavoritesHeader;
