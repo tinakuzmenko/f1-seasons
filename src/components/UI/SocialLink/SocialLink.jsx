2f8a6e67b364522f697a7084b3f6b18a65294175
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2022-01-31 18:21:46 +0100

    Fix link rel

--- src/components/UI/SocialLink/SocialLink.jsx
+++ src/components/UI/SocialLink/SocialLink.jsx
@@ -1,7 +1,7 @@
 const SocialLink = ({link, src, alt, width = 30, height = 30}) => {
   return (
-    <a href={link} target="_blank">
-      <img src={src} alt={alt} width={width} height={height} noopener="true"/>
+    <a href={link} target="_blank" noopener="true">
+      <img src={src} alt={alt} width={width} height={height}/>
     </a>
   )
 }
