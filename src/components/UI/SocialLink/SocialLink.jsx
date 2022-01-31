f4c3c2b533eae765dff58370b4f0c9135fc5352c
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2022-01-31 18:28:46 +0100

    SVG optimization

--- src/components/UI/SocialLink/SocialLink.jsx
+++ src/components/UI/SocialLink/SocialLink.jsx
@@ -1,6 +1,6 @@
 const SocialLink = ({link, src, alt, width = 30, height = 30}) => {
   return (
-    <a href={link} target="_blank" noopener="true">
+    <a href={link} target="_blank" rel="noopener noreferrer">
       <img src={src} alt={alt} width={width} height={height}/>
     </a>
   )
